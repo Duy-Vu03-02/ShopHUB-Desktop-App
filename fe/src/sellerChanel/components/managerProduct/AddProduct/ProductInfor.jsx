9fcd0550f837c627fbfc9edc772a6d9e013604f1
Duy-Vu03-02 <duyvu11122002@gmail.com>
2024-08-17 14:28:15 +0700

    done productInfor

--- fe/src/sellerChanel/components/managerProduct/AddProduct/ProductInfor.jsx
+++ fe/src/sellerChanel/components/managerProduct/AddProduct/ProductInfor.jsx
@@ -1,31 +1,26 @@
 import React, { useEffect, useState } from "react";
 
 const ProductInfor = React.memo(() => {
-  const [brand, setBrand] = useState("");
-  const [meterial, setMeterial] = useState("");
-  const [color, setColor] = useState("");
-  const [size, setSize] = useState("");
-  const [height, setHeight] = useState("");
-  const [width, setWidth] = useState("");
+  const [data, setData] = useState({
+    country: "",
+    brand: "",
+    material: "",
+    style: "",
+    sendFrom: "",
+    design: "",
+    data: "",
+  });
 
-  function handleChangeBrand(e) {
-    setBrand(e.target.value);
-  }
-  function handleChangeMeterial(e) {
-    setMeterial(e.target.value);
-  }
-  function handleChangeColor(e) {
-    setColor(e.target.value);
-  }
-  function handleChangeSize(e) {
-    setSize(e.target.value);
-  }
-  function handleChangeHeight(e) {
-    setHeight(e.target.value);
-  }
-  function handleChangeWidth(e) {
-    setWidth(e.target.value);
-  }
+  const handleChange = (e) => {
+    setData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  console.log(data);
 
   return (
     <>
@@ -36,33 +31,59 @@ const ProductInfor = React.memo(() => {
           <div className="left-infor">
             <div className="brand-infor input-detail-infor flex">
               <p>*Xuất xứ</p>
-              <input type="text" value={brand} onChange={handleChangeBrand} />
+              <input
+                type="text"
+                value={data.country}
+                onChange={handleChange}
+                name="country"
+              />
             </div>
             <div className="meterial-infor input-detail-infor flex">
               <p>Thương hiệu</p>
               <input
                 type="text"
-                value={meterial}
-                onChange={handleChangeMeterial}
+                value={data.brand}
+                onChange={handleChange}
+                name="brand"
               />
             </div>
             <div className="color-infor input-detail-infor flex">
               <p>Chất liệu</p>
-              <input type="text" value={color} onChange={handleChangeColor} />
+              <input
+                type="text"
+                value={data.material}
+                onChange={handleChange}
+                name="material"
+              />
             </div>
           </div>
           <div className="right-infor">
             <div className="size-infor input-detail-infor flex">
               <p>Phong cách</p>
-              <input type="text" value={size} onChange={handleChangeSize} />
+              <input
+                type="text"
+                value={data.style}
+                onChange={handleChange}
+                name="style"
+              />
             </div>
             <div className="height-infor input-detail-infor flex">
               <p>Gửi từ</p>
-              <input type="text" value={height} onChange={handleChangeHeight} />
+              <input
+                type="text"
+                value={data.sendFrom}
+                onChange={handleChange}
+                name="sendFrom"
+              />
             </div>
             <div className="width-infor input-detail-infor flex">
               <p>Kiểu dáng</p>
-              <input type="text" value={width} onChange={handleChangeWidth} />
+              <input
+                type="text"
+                value={data.design}
+                onChange={handleChange}
+                name="design"
+              />
             </div>
           </div>
         </div>
